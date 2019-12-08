50d11cd3cd3d5a0ccd5b0c74f3c1aa93abd6695d
Kelly Willard <32944774+kwillard95@users.noreply.github.com>
2019-12-08 11:22:05 -0800

    Consolidated About component
    Reduced number of lines I used for the About component

--- src/About.jsx
+++ src/About.jsx
@@ -9,17 +9,57 @@ class About extends React.Component {
       this.state = {
           readMore: false,
           showMore: false,
+          someCurrentData: {},
           currentData: {},
-          color: AboutStyle.color
+          color: '',
+          containerColor: '',
       }
       this.showClicked = this.showClicked.bind(this);
       this.readClicked = this.readClicked.bind(this);
   }
 
+  changeColor() {
+    var colors = [`#21CE99`, `#F45531`];
+    var color = colors[Math.round(Math.random() * (1 - 0) + 0)];
+    this.setState({color: color}); 
+    if (color === `#21CE99`) {
+        this.setState({containerColor: `rgb(33, 206, 153, .1)`}) 
+    } else {
+        this.setState({containerColor: `rgb(244, 85, 49, .1)`}) 
+    }
+}
+
+
   componentDidMount() {
+    this.changeColor();
     axios.get('/about/5de6414ac440ca30bd517220').then((response) => {
         response.data.adjectives = JSON.parse(response.data.adjectives);
-        this.setState({currentData: response.data})
+        const someData = {};
+
+       someData['CEO'] = response.data.CEO
+       someData['Employees'] = response.data.EmployeeCount
+       someData['Headquarters'] = response.data.Headquarters
+       someData['Founded'] = response.data.Founded
+       someData['Market Cap'] = response.data.MarketCap
+       someData['Price-Earnings Ratio'] = response.data.EarningsRatio
+       someData['Dividend Yield'] = response.data.DividendYield
+       someData['Average Volume'] = response.data.AvgVolume
+
+       const data = {};
+       for (var key in someData) {
+           data[key] = someData[key]
+       }
+       data['High Today'] = response.data.HighToday
+       data['Low Today'] = response.data.LowToday
+       data['Open Price'] = response.data.OpenPrice
+       data['Volume'] = response.data.Volume
+       data['52 Week High'] = response.data.HighYear
+       data['52 Week Low'] = response.data.LowYear
+       data.description = response.data.description
+       data.adjectives = response.data.adjectives
+    
+       this.setState({someCurrentData: someData, currentData: data})
+       
     }).catch((err) => {
         
     })
@@ -33,11 +73,21 @@ class About extends React.Component {
     this.setState({readMore: !this.state.readMore})
   }
 
+  mapObject(obj) {
+     return Object.keys(obj).map((key) => {
+        if (key === 'CEO') {
+            return <AboutStyle.GridItem><AboutStyle.InfoTitle>{key}</AboutStyle.InfoTitle><AboutStyle.InfoTextCEO style={{color: this.state.color}}>{obj.CEO}</AboutStyle.InfoTextCEO></AboutStyle.GridItem>
+         } else if (key !== '_id' && key !== 'description' && key !== 'adjectives' && key !== '__v') {
+            return <AboutStyle.GridItem><AboutStyle.InfoTitle>{key}</AboutStyle.InfoTitle><AboutStyle.InfoText>{obj[key]}</AboutStyle.InfoText></AboutStyle.GridItem>
+         }
+     })
+  }
+
   read() {
       if(!this.state.readMore) {
           if(this.state.currentData.description) {
               return(
-                <AboutStyle.AboutText>{this.state.currentData.description.split('.').splice(0,5).join('.')}. <AboutStyle.Read onClick={this.readClicked}>Read More</AboutStyle.Read></AboutStyle.AboutText>
+                <AboutStyle.AboutText>{this.state.currentData.description.split('.').splice(0,5).join('.')}. <AboutStyle.Read onClick={this.readClicked} style={{color: this.state.color}}>Read More</AboutStyle.Read></AboutStyle.AboutText>
               )
           } else {
               return(
@@ -46,61 +96,31 @@ class About extends React.Component {
           }
       } else {
           return(
-            <AboutStyle.AboutText>{this.state.currentData.description}<AboutStyle.Read onClick={this.readClicked}> Read Less</AboutStyle.Read></AboutStyle.AboutText>
+            <AboutStyle.AboutText>{this.state.currentData.description}<AboutStyle.Read onClick={this.readClicked} style={{color: this.state.color}}> Read Less</AboutStyle.Read></AboutStyle.AboutText>
           )
       }
   }
   show() {
-    
-      if (!this.state.showMore) {
-          return (
-              <div>
-                  <AboutStyle.Wrapper>
-                   <span><AboutStyle.AboutTitle>About</AboutStyle.AboutTitle> <AboutStyle.Show onClick={this.showClicked}>Show More</AboutStyle.Show></span>
-                   <AboutStyle.LineBreak />
-                   {this.read()}
-                   <AboutStyle.GridContainer>
-                   <AboutStyle.GridItem><AboutStyle.InfoTitle>CEO</AboutStyle.InfoTitle><AboutStyle.InfoTextCEO>{this.state.currentData.CEO}</AboutStyle.InfoTextCEO></AboutStyle.GridItem>
-                   <AboutStyle.GridItem><AboutStyle.InfoTitle>Employees</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.EmployeeCount}</AboutStyle.InfoText></AboutStyle.GridItem>
-                   <AboutStyle.GridItem><AboutStyle.InfoTitle>Headquarters</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.Headquarters}</AboutStyle.InfoText></AboutStyle.GridItem>
-                   <AboutStyle.GridItem><AboutStyle.InfoTitle>Founded</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.Founded}</AboutStyle.InfoText></AboutStyle.GridItem>
-                   <AboutStyle.GridItem><AboutStyle.InfoTitle>Market Cap</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.MarketCap}</AboutStyle.InfoText></AboutStyle.GridItem>
-                   <AboutStyle.GridItem><AboutStyle.InfoTitle>Price-Earnings Ratio</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.EarningsRatio}</AboutStyle.InfoText></AboutStyle.GridItem>
-                   <AboutStyle.GridItem><AboutStyle.InfoTitle>Dividend Yield</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.DividendYield}</AboutStyle.InfoText></AboutStyle.GridItem>
-                   <AboutStyle.GridItem><AboutStyle.InfoTitle>Average Volume</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.AvgVolume}</AboutStyle.InfoText></AboutStyle.GridItem>
-                   </AboutStyle.GridContainer>
-                   </AboutStyle.Wrapper>
-                   <Collections color={this.state.color} adj={this.state.currentData.adjectives}/>
-              </div>
-          )
-      } else {
-        return (
+    var data;
+    if (!this.state.showMore) {
+      data = this.state.someCurrentData;
+    } else {
+        data = this.state.currentData
+    }
+
+    return (
         <div>
-            <AboutStyle.Wrapper>
-            <span><AboutStyle.AboutTitle>About</AboutStyle.AboutTitle> <AboutStyle.Show onClick={this.showClicked}>Show Less</AboutStyle.Show></span>
-            <AboutStyle.LineBreak />
-            {this.read()}
-            <AboutStyle.GridContainer>
-            <AboutStyle.GridItem><AboutStyle.InfoTitle>CEO</AboutStyle.InfoTitle><AboutStyle.InfoTextCEO>{this.state.currentData.CEO}</AboutStyle.InfoTextCEO></AboutStyle.GridItem>
-            <AboutStyle.GridItem><AboutStyle.InfoTitle>Employees</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.EmployeeCount}</AboutStyle.InfoText></AboutStyle.GridItem>
-            <AboutStyle.GridItem><AboutStyle.InfoTitle>Headquarters</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.Headquarters}</AboutStyle.InfoText></AboutStyle.GridItem>
-            <AboutStyle.GridItem><AboutStyle.InfoTitle>Founded</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.Founded}</AboutStyle.InfoText></AboutStyle.GridItem>
-            <AboutStyle.GridItem><AboutStyle.InfoTitle>Market Cap</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.MarketCap}</AboutStyle.InfoText></AboutStyle.GridItem>
-            <AboutStyle.GridItem><AboutStyle.InfoTitle>Price-Earnings Ratio</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.EarningsRatio}</AboutStyle.InfoText></AboutStyle.GridItem>
-            <AboutStyle.GridItem><AboutStyle.InfoTitle>Dividend Yield</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.DividendYield}</AboutStyle.InfoText></AboutStyle.GridItem>
-            <AboutStyle.GridItem><AboutStyle.InfoTitle>Average Volume</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.AvgVolume}</AboutStyle.InfoText></AboutStyle.GridItem>
-            <AboutStyle.GridItem><AboutStyle.InfoTitle>High Today</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.HighToday}</AboutStyle.InfoText></AboutStyle.GridItem>
-            <AboutStyle.GridItem><AboutStyle.InfoTitle>Low Today</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.LowToday}</AboutStyle.InfoText></AboutStyle.GridItem>
-            <AboutStyle.GridItem><AboutStyle.InfoTitle>Open Price</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.OpenPrice}</AboutStyle.InfoText></AboutStyle.GridItem>
-            <AboutStyle.GridItem><AboutStyle.InfoTitle>Volume</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.Volume}</AboutStyle.InfoText></AboutStyle.GridItem>
-            <AboutStyle.GridItem><AboutStyle.InfoTitle>52 Week High</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.HighYear}</AboutStyle.InfoText></AboutStyle.GridItem>
-            <AboutStyle.GridItem><AboutStyle.InfoTitle>52 Week Low</AboutStyle.InfoTitle><AboutStyle.InfoText>{this.state.currentData.LowYear}</AboutStyle.InfoText></AboutStyle.GridItem>
-            </AboutStyle.GridContainer>
-            </AboutStyle.Wrapper>
-            <Collections color={this.state.color} adj={this.state.currentData.adjectives}/>
-        </div>
-        )
-      }
+                   <AboutStyle.Wrapper>
+                    <span><AboutStyle.AboutTitle>About</AboutStyle.AboutTitle> <AboutStyle.Show onClick={this.showClicked} style={{color: this.state.color}}>Show More</AboutStyle.Show></span>
+                    <AboutStyle.LineBreak />
+                    {this.read()}
+                    <AboutStyle.GridContainer>
+        {this.mapObject(data)}
+        </AboutStyle.GridContainer>
+                    </AboutStyle.Wrapper>
+                    <Collections color={this.state.color} container={this.state.containerColor} adj={this.state.currentData.adjectives}/>
+               </div>
+    )
   }
 
   render() {
